85a01c1db41c1bf3b3a5309f9149de4c231218c0
ashi-sahu <ashisahu1437@gmail.com>
2026-04-14 21:31:41 +0530

    Initialize Flask app with multiple routes

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Home Page
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+# Soil Info Page
+@app.route('/soil')
+def soil():
+    return render_template("soil.html")
+
+# Crop Info Page
+@app.route('/crop')
+def crop():
+    return render_template("crop.html")
+
+# Recommendation Page
+@app.route('/recommend', methods=['GET', 'POST'])
+def recommend():
+    result = None
+
+    if request.method == 'POST':
+        soil = int(request.form['soil'])
+        temp = float(request.form['temperature'])
+        humidity = float(request.form['humidity'])
+        rainfall = float(request.form['rainfall'])
+
+        # Simple Rule-Based Logic (Easy + Works Perfect)
+        if soil == 0 and rainfall > 200:
+            result = "Rice"
+        elif soil == 1 and temp > 25:
+            result = "Cotton"
+        elif soil == 2 and humidity < 60:
+            result = "Groundnut"
+        elif soil == 3:
+            result = "Tea"
+        else:
+            result = "Maize"
+
+    return render_template("recommend.html", result=result)
+
+# Run App
+if __name__ == "__main__":  
+    app.run(debug=True)
